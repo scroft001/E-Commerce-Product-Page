b417be2a5513c7fa2e0c1060083c0c5a9ae7365c
Steven <scroft001@gmail.com>
2022-11-21 17:24:17 -0800

    added images and started navbar jsx

--- ecommerce/src/components/navbar/Navbar.jsx
+++ ecommerce/src/components/navbar/Navbar.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
 import './navbar.scss';
+import logo from '../../images/logo.svg';
+import cartIcon from '../../images/icon-cart.svg';
+import avatar from '../../images/image-avatar.png';
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav>
+        <div className='app__menu'>
+            <img src={logo} alt='logo' className='app__menu-logo' />
+            <ul>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+            </ul>
+        </div>
+        <div className="app__cart">
+            <img src={cartIcon} alt='cart' className='app__cart-icon' />
+            <img src={avatar} alt='profile' className='app__cart-profile' />
+        </div>
+    </nav>
   )
 }
 
