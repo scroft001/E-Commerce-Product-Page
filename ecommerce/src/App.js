63c307a3e0238bd17e2dca08332443e9f6ccea53
Steven <scroft001@gmail.com>
2022-11-21 16:49:26 -0800

    project created and sass installed

---
+++ ecommerce/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import {Navbar, Product} from './components';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Product />
+    </div>
+  );
+}
+
+export default App;
---
+++ ecommerce/src/components/index.js
@@ -0,0 +1,5 @@
+export {default as Cart} from './cart/Cart';
+export {default as Lightbox} from './lightbox/Lightbox';
+export {default as Navbar} from './navbar/Navbar';
+export {default as Product} from './product/Product';
+export {default as Purchase} from './purchase/Purchase';
