236e33e58687d564905cf8117a55b0a62b0d9c01
Steven <scroft001@gmail.com>
2022-11-23 17:34:49 -0800

    Started product section

--- ecommerce/src/components/product/Product.jsx
+++ ecommerce/src/components/product/Product.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
+import { Purchase } from '..'
+import product1 from '../../images/image-product-1.jpg';
+import product1Thumb from '../../images/image-product-1-thumbnail.jpg';
+import product2 from '../../images/image-product-2.jpg';
+import product2Thumb from '../../images/image-product-2-thumbnail.jpg';
+
+
+const heroImages = [product1, product2];
+const thumbImages = [product1Thumb, product2Thumb];
 
 const Product = () => {
   return (
-    <div>Product</div>
+    <main className='app__product'>
+      <section className='app__product-images'>
+        <div className='main-image'>
+          {/* <img src={} alt={} /> */}
+        </div>
+        <div className='image-selector'>
+
+        </div>
+      </section>
+      <section className='app__product-description'>
+        {/* <Purchase /> */}
+      </section>
+    </main>
   )
 }
 
