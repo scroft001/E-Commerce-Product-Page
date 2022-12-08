a0ddbecc27f1fa6995fb484666025ca957e4679a
Steven <scroft001@gmail.com>
2022-12-07 16:48:11 -0800

    Created image selector component for modularity

--- ecommerce/src/components/index.js
+++ ecommerce/src/components/index.js
@@ -3,3 +3,4 @@ export {default as Lightbox} from './lightbox/Lightbox';
 export {default as Navbar} from './navbar/Navbar';
 export {default as Product} from './product/Product';
 export {default as Purchase} from './purchase/Purchase';
+export {default as ImageSelector} from './imageSelector/ImageSelelctor';
